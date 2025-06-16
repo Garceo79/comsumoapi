e33997e86d005a4937e8677a88d73156a8455dbd
Sinoe Geovanni Arceo López <155335254+Garceo79@users.noreply.github.com>
2025-06-16 16:58:29 -0600

    Add files via upload

---
+++ src/main/java/com/projects/comsumoapi/ComsumoapiApplication.java
@@ -0,0 +1,13 @@
+package com.projects.comsumoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComsumoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComsumoapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projects/comsumoapi/ComsumoapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projects.comsumoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComsumoapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
